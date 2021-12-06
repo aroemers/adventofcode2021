956ab65a4c2fc9c691f38fa7e1ecccd16ad48704
Arnout Roemers <roemers@zorgdomein.nl>
2021-12-06 12:14:39 +0100

    Day 06 - replace Hash with Array
    Now the Array#rotate can be used

--- day06/day06.rb
+++ day06/day06.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 def read_input(filename)
-  state = File.read(filename).split(',').collect(&:to_i).tally
-  state.default = 0
-  state
+  tallied = File.read(filename).split(',').collect(&:to_i).tally
+  (0..8).collect { |timer| tallied[timer] || 0 }
 end
 
 def solve(filename, days)
   state = read_input filename
   days.times do
-    spawn = state[0]
-    (0..7).each { |timer| state[timer] = state[timer + 1] }
-    state[8] = spawn
-    state[6] += spawn
+    state = state.rotate
+    state[6] += state[8]
   end
-  state.values.sum
+  state.sum
 end
